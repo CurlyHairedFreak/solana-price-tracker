674ef768966e4c4da708ba97a624d5b16e0a7b38
CurlyHairedFreak <152513491+CurlyHairedFreak@users.noreply.github.com>
2023-12-24 19:59:03 +0000

    Third commit
    Fix image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function Header() {
   return (
     <header>
-      <img src="/solana-logo.png" alt="Solana-logo" className="header--img" />
+      <img
+        src="https://solana.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogotype.e4df684f.svg&w=256&q=75"
+        alt="Solana-logo"
+        className="header--img"
+      />
       <h2 className="header--title">Price Tracker</h2>
     </header>
   );
